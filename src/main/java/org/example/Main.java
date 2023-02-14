d4035e84b5a2894e1f396733f3956b2b86b80eac
Raghu D <raghudwork1@gmail.com>
2023-02-14 21:03:04 +0530

    lot of bugs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helo world!");
+        System.out.println("Helo world!");
+        System.out.println("Helo world!");
+        System.out.println("Helo world!");
+
     }
 }
